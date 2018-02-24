94bd50217ee3cf5842f9e5053f018c53247ab66f
ms <mschultz@hawaii.edu>
2018-02-24 13:52:08 -1000

    edit antlr.py for testing; currently throws error from SQLiteParser.py

--- sql/python/antlr.py
+++ sql/python/antlr.py
@@ -4,6 +4,10 @@ from SQLiteLexer import SQLiteLexer
 from SQLiteParser import SQLiteParser
 # from HtmlSQLiteListener import HtmlSQLiteListener
  
+def getTableName(sqlString):
+    tableName = ""
+    return tableName
+
 def main(argv):
     input = FileStream(argv[1])
     lexer = SQLiteLexer(input)
@@ -18,6 +22,10 @@ def main(argv):
 #    walker.walk(htmlSQLite, tree)
         
     output.close()      
+
+def test(argv):
+    print("this is a test")
  
 if __name__ == '__main__':
-    main(sys.argv)
+#    main(sys.argv)
+    test(sys.argv)
